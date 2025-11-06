683e06df1273ebf961f15b5d18f53e2d40e15f16
Rabeea Shahid <134997377+Rabeea-S@users.noreply.github.com>
2025-11-05 23:12:17 -0700

    more rearranging

--- app/src/main/java/com/example/lotteryevent/organizer/ConfirmEntrantsNotifyFragment.java
+++ app/src/main/java/com/example/lotteryevent/organizer/ConfirmEntrantsNotifyFragment.java
@@ -1,7 +1,6 @@
-package com.example.lotteryevent;
+package com.example.lotteryevent.organizer;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,12 +10,13 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.lotteryevent.NotificationCustomManager;
+import com.example.lotteryevent.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
