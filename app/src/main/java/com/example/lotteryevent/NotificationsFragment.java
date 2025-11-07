5481188dd2d14df74b3af9e2d47060d6cb92bbce
Rabeea Shahid <134997377+Rabeea-S@users.noreply.github.com>
2025-11-06 21:40:42 -0700

    setting up notifications fragment

--- app/src/main/java/com/example/lotteryevent/NotificationsFragment.java
+++ app/src/main/java/com/example/lotteryevent/NotificationsFragment.java
@@ -1,6 +1,53 @@
 package com.example.lotteryevent;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class NotificationsFragment extends Fragment {
+    private FirebaseFirestore db;
+    private RecyclerView recyclerView;
+
+    /**
+     *
+     * @param inflater The LayoutInflater object that can be used to inflate
+     * any views in the fragment,
+     * @param container If non-null, this is the parent view that the fragment's
+     * UI should be attached to.  The fragment should not add the view itself,
+     * but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     *
+     * @return
+     */
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_notifications, container, false);
+    }
+
+    /**
+     *
+     * @param view The View returned by {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     */
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        db = FirebaseFirestore.getInstance();
+
+        recyclerView = view.findViewById(R.id.notifications_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+    }
 }
