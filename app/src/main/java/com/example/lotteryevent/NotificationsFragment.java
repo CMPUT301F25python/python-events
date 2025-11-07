a755fa4d1a36ef39ff87a251e08d78afd4112752
Rabeea Shahid <134997377+Rabeea-S@users.noreply.github.com>
2025-11-07 01:46:10 -0700

    order notifs from latest to oldest

--- app/src/main/java/com/example/lotteryevent/NotificationsFragment.java
+++ app/src/main/java/com/example/lotteryevent/NotificationsFragment.java
@@ -27,6 +27,7 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
@@ -81,6 +82,7 @@ public class NotificationsFragment extends Fragment {
         if (user != null) {
             String uid = user.getUid();
             db.collection("notifications").whereEqualTo("recipientId", uid)
+                .orderBy("timestamp", Query.Direction.DESCENDING)
                 .addSnapshotListener(new EventListener<QuerySnapshot>() {
                     @Override
                     public void onEvent(@Nullable QuerySnapshot value,
