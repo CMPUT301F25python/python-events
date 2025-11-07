9a1c04a86dbed85d4e83b12aa7dd0b8fd883557e
Rabeea Shahid <134997377+Rabeea-S@users.noreply.github.com>
2025-11-07 08:12:26 -0700

    changes a mesasge formatter to static method, addes unit tests for it

---
+++ app/src/test/java/com/example/lotteryevent/NotificationAdapterTest.java
@@ -0,0 +1,74 @@
+package com.example.lotteryevent;
+
+import static org.junit.Assert.assertEquals;
+
+import com.example.lotteryevent.adapters.NotificationAdapter;
+import com.example.lotteryevent.data.Notification;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ * Unit tests for the validation logic within the {@link CreateEventFragment}.
+ * <p>
+ * This class tests the {@code validateEventInput} method to ensure all business rules
+ * for creating an event are correctly enforced. It covers scenarios such as empty fields,
+ * chronological order of dates, and the integrity of the registration period.
+ */
+public class NotificationAdapterTest {
+
+    private Notification notification;
+
+    private String uid;
+    private String title;
+    private String message;
+    private String type;
+    private String eventId;
+    private String eventName;
+    private String organizerId;
+    private String organizerName;
+
+    @Before
+    public void setUp() {
+        uid = "Wa54uQ98ykOHpRq0IbZtMJoHAkU2";
+        title = "Test Title";
+        message = "Test Message";
+        type = "lottery_win";
+        eventId = "2HY5fqDbrNjxiTGjNh8J";
+        eventName = "Event Name";
+        organizerId = "aIJocfaDTrWhIp3N3FjvADBbTuv";
+        organizerName = "Organizer Name";
+        notification = new Notification(uid, title, message, type, eventId, eventName, organizerId, organizerName);
+    }
+
+    // All Valid Scenarios
+    @Test
+    public void testMessageConstructorLotteryWin() {
+        notification.setType("lottery_win");
+        String message = NotificationAdapter.messageConstructor(notification);
+        assertEquals(message, "\uD83C\uDF89 You've been selected for " + notification.getEventName() + "! Tap to accept or decline.");
+    }
+
+    @Test
+    public void testMessageConstructorLotteryLoss() {
+        notification.setType("lottery_loss");
+        String message = NotificationAdapter.messageConstructor(notification);
+        assertEquals(message, "❌ You weren't selected for " + notification.getEventName() + ".");
+    }
+
+    @Test
+    public void testMessageConstructorEventUpdate() {
+        notification.setType("event_update");
+        String message = NotificationAdapter.messageConstructor(notification);
+        assertEquals(message, "\uD83D\uDD01 A spot just opened for " + notification.getEventName() + "!");
+    }
+
+    @Test
+    public void testMessageConstructorCustomMessage() {
+        notification.setType("custom_message");
+        String message = NotificationAdapter.messageConstructor(notification);
+        assertEquals(message, "\uD83D\uDCAC Message from the organizer of " + notification.getEventName() + ": " + notification.getMessage());
+    }
+
+}
