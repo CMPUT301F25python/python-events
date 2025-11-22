e991bb3b1eb7a6b263db43ec210ad99263558580
Sanaa <sbhaidani04@gmail.com>
2025-11-22 10:50:57 -0700

    added javadocs

--- app/src/main/java/com/example/lotteryevent/ui/LotteryGuidelinesFragment.java
+++ app/src/main/java/com/example/lotteryevent/ui/LotteryGuidelinesFragment.java
@@ -12,11 +12,26 @@ import android.view.ViewGroup;
 
 import com.example.lotteryevent.R;
 
+/**
+ * A {@link Fragment} subclass that allows users to view the lottery guidelines.
+ * Its sole responsibility is to display the data.
+ * @author Sanaa Bhaidani
+ * @version 1.0
+ */
 public class LotteryGuidelinesFragment extends Fragment {
 
+    /**
+     * Inflates the fragment's layout.
+     *
+     * @param inflater The LayoutInflater object to inflate views.
+     * @param container The parent view that the fragment's UI should be attached to.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed from a previous saved state.
+     * @return The View for the fragment's UI.
+     */
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        // inflate layout for this fragment
         return inflater.inflate(R.layout.fragment_lottery_guidelines, container, false);
     }
 
