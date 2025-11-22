d774911c24adff0cb2c177440ef8380ce104014b
Sanaa <sbhaidani04@gmail.com>
2025-11-22 10:47:18 -0700

    removed viewmodel since not used

--- app/src/main/java/com/example/lotteryevent/ui/LotteryGuidelinesFragment.java
+++ app/src/main/java/com/example/lotteryevent/ui/LotteryGuidelinesFragment.java
@@ -1,7 +1,5 @@
 package com.example.lotteryevent.ui;
 
-import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -13,27 +11,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.lotteryevent.R;
-import com.example.lotteryevent.viewmodels.LotteryGuidelinesViewModel;
 
 public class LotteryGuidelinesFragment extends Fragment {
-
-    private LotteryGuidelinesViewModel mViewModel;
-
-    public static LotteryGuidelinesFragment newInstance() {
-        return new LotteryGuidelinesFragment();
-    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_lottery_guidelines, container, false);
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(LotteryGuidelinesViewModel.class);
-        // TODO: Use the ViewModel
     }
 
 }
--- app/src/main/java/com/example/lotteryevent/viewmodels/LotteryGuidelinesViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.lotteryevent.viewmodels;
-
-import androidx.lifecycle.ViewModel;
-
-public class LotteryGuidelinesViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
