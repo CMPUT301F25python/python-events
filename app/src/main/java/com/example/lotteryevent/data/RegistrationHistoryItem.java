4a6c4c5af56a01f4deaa55356519af98c2756ef9
Sanaa <sbhaidani04@gmail.com>
2025-11-23 13:18:20 -0700

    generated javadocs for reghistoryitem

--- app/src/main/java/com/example/lotteryevent/data/RegistrationHistoryItem.java
+++ app/src/main/java/com/example/lotteryevent/data/RegistrationHistoryItem.java
@@ -1,5 +1,9 @@
 package com.example.lotteryevent.data;
 
+/**
+ * This class creates item that will represent each row in the registration history fragment.
+ * This will be displayed in the dynamic container.
+ */
 public class RegistrationHistoryItem {
     private String eventId;
     private String eventName;
@@ -14,10 +18,10 @@ public class RegistrationHistoryItem {
 
     /**
      * This constructor initializes the instances of the four parameters
-     * @param eventId the event ID
-     * @param eventName the event name
-     * @param label the status assigned to the user based on draw results and displayed in UI
-     * @param selected whether or not the user won the lottery
+     * @param eventId the event ID as a String
+     * @param eventName the event name as a String
+     * @param label the status assigned to the user based on draw results and displayed in UI as a String
+     * @param selected whether or not the user won the lottery as a Boolean
      */
     public RegistrationHistoryItem(String eventId, String eventName, String label, boolean selected) {
         this.eventId = eventId;
@@ -26,18 +30,34 @@ public class RegistrationHistoryItem {
         this.selected = selected;
     }
 
+    /**
+     * Returns the unique identifier associated with the event.
+     * @return the event ID as a String
+     */
     public String getEventId() {
         return eventId;
     }
 
+    /**
+     * Returns the name associated with the event.
+     * @return the name as a String
+     */
     public String getEventName() {
         return eventName;
     }
 
+    /**
+     * Returns the label assigned to the entrant based on draw results.
+     * @return the assigned label as a String
+     */
     public String getLabel() {
         return label;
     }
 
+    /**
+     * Returns whether or not an entrant was selected
+     * @return value of selected as a Boolean
+     */
     public boolean isSelected() {
         return selected;
     }
