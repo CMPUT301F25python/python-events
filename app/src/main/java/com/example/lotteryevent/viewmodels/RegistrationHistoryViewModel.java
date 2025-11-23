79987aeb3555d50ea4314a92c23ad433edc23f68
Sanaa <sbhaidani04@gmail.com>
2025-11-23 14:45:16 -0700

    set up viewmodel

--- app/src/main/java/com/example/lotteryevent/viewmodels/RegistrationHistoryViewModel.java
+++ app/src/main/java/com/example/lotteryevent/viewmodels/RegistrationHistoryViewModel.java
@@ -1,7 +1,30 @@
 package com.example.lotteryevent.viewmodels;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.lotteryevent.data.RegistrationHistoryItem;
+import com.example.lotteryevent.repository.IRegistrationHistoryRepository;
+
+import java.util.List;
+
+/**
+ * This viewmodel exposes the user's event history.
+ */
 public class RegistrationHistoryViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    private final IRegistrationHistoryRepository historyRepository;
+    private final LiveData<List<RegistrationHistoryItem>> userHistory;
+
+    public RegistrationHistoryViewModel(IRegistrationHistoryRepository historyRepository){
+        this.historyRepository = historyRepository;
+        this.userHistory = historyRepository.fetchRegistrationHistory();
+    }
+
+    public LiveData<List<RegistrationHistoryItem>> getUserHistory() {
+        return userHistory;
+    }
+
+    public LiveData<String> getUserMessage(){
+        return historyRepository.getUserMessage();
+    }
 }
