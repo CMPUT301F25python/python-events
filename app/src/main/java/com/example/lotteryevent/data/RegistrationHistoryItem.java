4cc5234af258eee250115eb6a2b49a4e9a28582f
Sanaa <sbhaidani04@gmail.com>
2025-11-23 13:37:12 -0700

    updated Item to be simpler + include setters

--- app/src/main/java/com/example/lotteryevent/data/RegistrationHistoryItem.java
+++ app/src/main/java/com/example/lotteryevent/data/RegistrationHistoryItem.java
@@ -1,33 +1,18 @@
 package com.example.lotteryevent.data;
 
 /**
- * This class creates item that will represent each row in the registration history fragment.
+ * This class creates an item that will represent each row in the registration history fragment.
  * This will be displayed in the dynamic container.
  */
 public class RegistrationHistoryItem {
     private String eventId;
     private String eventName;
-    private String label; // (selected, not selected, waiting)
-    private boolean selected; // flag (true if selected)
+    private String status; // (waiting = not selected, invited = selected)
 
     /**
      * A public no-argument constructor is required for Firestore deserialization.
      */
     private RegistrationHistoryItem(){
-    }
-
-    /**
-     * This constructor initializes the instances of the four parameters
-     * @param eventId the event ID as a String
-     * @param eventName the event name as a String
-     * @param label the status assigned to the user based on draw results and displayed in UI as a String
-     * @param selected whether or not the user won the lottery as a Boolean
-     */
-    public RegistrationHistoryItem(String eventId, String eventName, String label, boolean selected) {
-        this.eventId = eventId;
-        this.eventName = eventName;
-        this.label = label;
-        this.selected = selected;
     }
 
     /**
@@ -39,6 +24,14 @@ public class RegistrationHistoryItem {
     }
 
     /**
+     * Sets the eventId
+     * @param eventId the event ID as a String
+     */
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    /**
      * Returns the name associated with the event.
      * @return the name as a String
      */
@@ -47,18 +40,26 @@ public class RegistrationHistoryItem {
     }
 
     /**
-     * Returns the label assigned to the entrant based on draw results.
-     * @return the assigned label as a String
+     * Sets the eventName
+     * @param eventName the name as a String
      */
-    public String getLabel() {
-        return label;
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
     }
 
     /**
-     * Returns whether or not an entrant was selected
-     * @return value of selected as a Boolean
+     * Returns the status assigned to the entrant based on draw results.
+     * @return the assigned status as a String
      */
-    public boolean isSelected() {
-        return selected;
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * Sets the status of the draw
+     * @param status the user's draw status as a String
+     */
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
